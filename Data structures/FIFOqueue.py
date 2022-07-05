d9c2f507c0329b538ee8a7bd4fd2b020181a90f0
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2022-07-05 01:29:14 -0300

    Queue data structure - FIFO

---
+++ Data structures/FIFOqueue.py
@@ -0,0 +1,30 @@
+class Fila:
+    def __init__(self):
+        self.queue = []
+
+    def addIn(self, item):
+        self.queue.append(item)
+
+    def remove(self):
+        try:
+            self.queue.pop(self.queue.index(self.queue[0]))
+        except IndexError:
+            return "Não existe elemento a ser removido na posição indicada!"
+
+    def removeFrom(self, pos):
+        try:
+            self.queue.pop(self.queue.index(self.queue[pos]))
+        except IndexError:
+            return "Não existe elemento a ser removido na posição indicada!"
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def clear(self):
+        self.queue = []
+    
+    def addArray(self, array):
+        self.queue.extend(array)
+
+    def queueLenght(self):
+        return len(self.queue)
